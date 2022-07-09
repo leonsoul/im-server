d417578b89e5f035135fca54df2cbd5c32934b3a
Bene <wangben@alltuu.com>
2022-07-10 01:04:23 +0800

    项目测试运行

--- distribution/src/main/scripts/wildfirechat.sh
+++ distribution/src/main/scripts/wildfirechat.sh
@@ -87,8 +87,8 @@ JAVA_OPTS="$JAVA_OPTS -XX:MaxGCPauseMillis=500"
 echo "警告：没有设置JVM内存参数！"
 echo "请设置JVM参数Xmx和Xms，设置为您为IM服务预留的内存大小，注意需要刨除操作系统占用，如果有其它系统也需要相应去除占用。"
 echo ""
-#JAVA_OPTS="$JAVA_OPTS -Xmx2G"
-#JAVA_OPTS="$JAVA_OPTS -Xms2G"
+JAVA_OPTS="$JAVA_OPTS -Xmx12G"
+JAVA_OPTS="$JAVA_OPTS -Xms12G"
 
 
 $JAVA -server $JAVA_OPTS $JAVA_OPTS_SCRIPT -Dlog4j.configurationFile="file:$LOG_FILE" -Dlog4j2.formatMsgNoLookups=true -Dcom.mchange.v2.c3p0.cfg.xml="$C3P0_CONF_FILE" -Dhazelcast.configuration="file:$HZ_CONF_FILE" -Dwildfirechat.path="$WILDFIRECHAT_PATH" -cp "$WILDFIRECHAT_HOME/lib/*" cn.wildfirechat.server.Server
